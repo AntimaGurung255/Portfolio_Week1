293ccb56b2934215d14442fb7d6714898c5ec081
AntimaGurung255 <u2575228@uel.ac.uk>
2024-12-25 18:05:49 +0545

    Add files via upload

---
+++ week 1/index.js
@@ -0,0 +1,14 @@
+//console.log("this is my first visual code")
+//console.log("congrates your first code is successful")
+
+//example 2
+const num4=15;
+const num5= 10;
+const sum = num4+num5;
+console.log(" The Addition of numbers = " +sum)
+const sub = num4-num5;
+console.log(" The subtraction of numbers = " +sub)
+const multiply = num4*num5;
+console.log(" The multiply of numbers = " +multply)
+const div = num4/num5;
+console.log(" The div of numbers = " +div)
